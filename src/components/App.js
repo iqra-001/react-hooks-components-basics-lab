57d4c46bc1d451da069a43a626448e98d73a80b5
iqra <abdikarimiqra@gmail.com>
2025-04-10 20:37:21 +0300

    index.html

--- src/components/App.js
+++ src/components/App.js
@@ -16,13 +16,17 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About(){
+  return <div id= "about"> <h2> About </h2></div>
+  /* write an <About> component here */}
 
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home/>
+      <About/>
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
